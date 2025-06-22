44d218c74724952c0254ba15422a985eb7ce6ce0
hrithik210 <hrithiknayak7829@gmail.com>
2025-06-22 23:58:25 +0530

    added text splitter

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 loader = PyPDFLoader('Holiday Calendar-25.pdf')
+splitter = RecursiveCharacterTextSplitter(
+  chunk_size = 100,
+  chunk_overlap = 50,
+)
 
 pages = loader.load()
 
