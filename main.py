e2dcefc7302281dc1c6afe0980920855045d1c95
hrithik210 <hrithiknayak7829@gmail.com>
2025-06-23 00:48:32 +0530

    Added embeddings and faiss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import FakeEmbeddings
+from langchain.vectorstores import FAISS
+
 
 
 loader = PyPDFLoader('Holiday Calendar-25.pdf')
@@ -9,5 +12,14 @@ splitter = RecursiveCharacterTextSplitter(
 )
 
 pages = loader.load()
-
 print(pages[0].page_content)
+
+docs = splitter.split_documents(pages)
+print("docs : " , docs)
+
+
+#Emeddings
+embeddings = FakeEmbeddings(size = 1536)
+
+db = FAISS.from_documents(docs, embeddings)
+print(f"📦 FAISS vector store created with {db.index.ntotal} chunks.")
